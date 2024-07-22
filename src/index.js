3473d22084ead1efa591be4c8f4c810f48aa6051
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2024-07-22 23:29:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/setparam">
+    <BrowserRouter basename="/replaceparams">
      <App />
     </BrowserRouter>
   </React.StrictMode>
