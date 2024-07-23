2adafbed9aa7e8eb1cbcc39d70df1fc30c348caa
anlethanh048 <57203593+anlethanh048@users.noreply.github.com>
2024-07-23 22:01:38 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename="/replaceparams">
-     <h1>Lê Thanh Ân</h1>
+     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
